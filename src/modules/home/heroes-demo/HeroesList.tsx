76369bfd7e415f6d194b3b19e733c48b94d2b31a
diosyahrizal <diosyahrizal@gmail.com>
2019-03-09 17:28:27 +0700

    styling details

--- src/modules/home/heroes-demo/HeroesList.tsx
+++ src/modules/home/heroes-demo/HeroesList.tsx
@@ -21,6 +21,7 @@ interface HeroesListProps {
 
 interface HomeFirstPageState {
   open: boolean;
+  heroSelection?: Hero;
 }
 
 const API_ENDPOINT = process.env.REACT_APP_API_ENDPOINT || 'https://api.opendota.com';
@@ -49,37 +50,33 @@ class HeroesList extends React.Component<HeroesListProps, HomeFirstPageState> {
       <>
         <DrawerHeader title="Drawer Title" />
         <DrawerBody>
-          <p>
-            Congratulations! You have opened this drawer.{' '}
-            <a href="https://www.youtube.com/watch?v=ctSYCoMF4z4" target="_blank" rel="noopener noreferrer">
-              Have some music.
-            </a>
-          </p>
-
-          <p>
-            <a href="https://www.youtube.com/watch?v=mUGDxyG1kOI" target="_blank" rel="noopener noreferrer">
-              Lorem ipsum dolor sit amet
-            </a>
-            , consectetur adipiscing elit. Aliter enim nosmet ipsos nosse non possumus. Quae ista amicitia est? Duo
-            Reges: constructio interrete. An hoc usque quaque, aliter in vita? Primum quid tu dicis breve? Cum ageremus,
-            inquit, vitae beatum et eundem supremum diem, scribebamus haec. Sed finge non solum callidum eum, qui
-            aliquid improbe faciat, verum etiam praepotentem, ut M. <i>Tibi hoc incredibile, quod beatissimum.</i>{' '}
-          </p>
-
-          <p>
-            Quis animo aequo videt eum, quem inpure ac flagitiose putet vivere? Etsi ea quidem, quae adhuc dixisti,
-            quamvis ad aetatem recte isto modo dicerentur. Non minor, inquit, voluptas percipitur ex vilissimis rebus
-            quam ex pretiosissimis. Illa tamen simplicia, vestra versuta. Si qua in iis corrigere voluit, deteriora
-            fecit. Stuprata per vim Lucretia a regis filio testata civis se ipsa interemit.{' '}
-          </p>
-
-          <p>
-            Huic ego, si negaret quicquam interesse ad beate vivendum quali uteretur victu, concederem, laudarem etiam;
-            Est enim effectrix multarum et magnarum voluptatum. At miser, si in flagitiosa et vitiosa vita afflueret
-            voluptatibus. At iste non dolendi status non vocatur voluptas. Nunc ita separantur, ut disiuncta sint, quo
-            nihil potest esse perversius. <i>Hic nihil fuit, quod quaereremus.</i> <b>Frater et T.</b> Ergo id est
-            convenienter naturae vivere, a natura discedere. Si quae forte-possumus.{' '}
-          </p>
+          {this.state.heroSelection ? (
+            <div>
+              <div style={styles.container}>
+                <img src={API_ENDPOINT + this.state.heroSelection.img} style={styles.image} />
+                <h1 style={styles.name}>{this.state.heroSelection.localized_name}</h1>
+              </div>
+              <img src={API_ENDPOINT + this.state.heroSelection.icon} alt="" />
+              <p>
+                <b>Type: </b>
+                {this.state.heroSelection.attack_type}
+              </p>
+              <p>
+                <b>Roles: </b>
+                {this.state.heroSelection.roles.join(', ')}
+              </p>
+              <CardInfo>
+                <CardInfoKey>Primary Attr</CardInfoKey>
+                <CardInfoValue>{this.state.heroSelection.primary_attr}</CardInfoValue>
+              </CardInfo>
+              <CardInfo>
+                <CardInfoKey>Legs</CardInfoKey>
+                <CardInfoValue>{this.state.heroSelection.legs}</CardInfoValue>
+              </CardInfo>
+            </div>
+          ) : (
+            ''
+          )}
         </DrawerBody>
         <DrawerFooter>
           <Button color="primary" onClick={() => this.toggleDrawer()}>
@@ -127,6 +124,10 @@ class HeroesList extends React.Component<HeroesListProps, HomeFirstPageState> {
           key={hero.id}
           title={hero.localized_name}
           avatarComponent={<Avatar src={API_ENDPOINT + hero.img} size={40} />}
+          onClick={() => {
+            this.toggleDrawer();
+            this.setState({ heroSelection: hero });
+          }}
         >
           <div className="mb-2">
             {hero.attack_type} - <span>{hero.roles.join(', ')}</span>
@@ -144,5 +145,23 @@ class HeroesList extends React.Component<HeroesListProps, HomeFirstPageState> {
     });
   }
 }
+
+const styles = {
+  container: {
+    display: 'flex',
+    alignItems: 'center',
+    flexDirection: 'column'
+  } as React.CSSProperties,
+
+  name: {
+    marginBottom: '40px'
+  },
+
+  image: {
+    flex: 1,
+    justifyContent: 'center',
+    borderRadius: 50
+  }
+};
 
 export default HeroesList;
